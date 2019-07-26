82783f2e00ae138fbb581c9a8a13b7c59f9e33a9
Kirill Kouzoubov <kirill888@gmail.com>
2019-07-26 13:07:30 +1000

    Auto-remove docker container after run

--- run.sh
+++ run.sh
@@ -4,16 +4,11 @@ SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 cd "${SDIR}"
 
-cc_name="${1:-kk-geo-build}"
-
 mkdir -p build
 mkdir -p dl
 
-docker rm "${cc_name}" 2> /dev/null || true
-
 docker run \
-       --name "${cc_name}" \
        -v $(pwd)/dl:/dl \
        -v $(pwd)/build:/build \
-       -ti \
+       -ti --rm \
        kkodc/geo-builder $@
