9e7adafb4b6278a83bbd8b758cb6b0eb7bfdcaa1
Kirill Kouzoubov <kirill888@gmail.com>
2019-07-31 16:53:12 +1000

    Only build base images locally when asked

--- build-docker.sh
+++ build-docker.sh
@@ -5,8 +5,11 @@ set -eu
 SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 cd "${SDIR}"
-docker build \
-       --tag kkodc/geo-builder:proj5 ./base
+
+[ "${1-:}" == "base" ] && {
+    # by default pull base from docker hub
+    docker build --tag kkodc/geo-builder:proj5 ./base
+}
 
 docker build \
        --build-arg USER_NAME=$USER \
