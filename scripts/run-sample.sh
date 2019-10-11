e4eeb4cda21464ec62e616ca2fda2537dac73bea
Kirill Kouzoubov <kirill888@gmail.com>
2019-10-11 11:56:41 +1100

    Add mechanism to access local db from within docker run
    - Make sure user is the same inside docker as outside
    - Expose /run/postgresql directory (for access to db outside of docker)
    - Supply DB_DATABASE=datacube env option when running
    
    Also move user name/id related args down in Docker to help with caching

--- scripts/run-sample.sh
+++ scripts/run-sample.sh
@@ -5,7 +5,9 @@ nb_dir="${1:-$(pwd)}"
 nb_port="${2:-9988}"
 
 docker run \
-       -v ${nb_dir}:/nb \
-       -p ${nb_port}:9988 \
        -ti --rm \
-       geobase:local $@
+       -v ${nb_dir}:/nb \
+       -v /run/postgresql:/run/postgresql \
+       -e DB_DATABASE=datacube \
+       -p ${nb_port}:9988 \
+       geobase:local_sample $@
