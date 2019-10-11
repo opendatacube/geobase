91e55f2cd23bc7ee1fe4bc90c33d1ec87fd2d0a7
Kirill Kouzoubov <kirill888@gmail.com>
2019-10-11 11:56:41 +1100

    Add mechanism to access local db from within docker run
    - Make sure user is the same inside docker as outside
    - Expose /run/postgresql directory (for access to db outside of docker)
    - Supply DB_DATABASE=datacube env option when running
    
    Also move user name/id related args down in Docker to help with caching

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -9,7 +9,15 @@ set -eu
     docker build --tag "kkodc/geobase:runner" ./base/runner
 }
 
+[ "${1-:}" == "sample" ] && {
+    docker build \
+           --build-arg nb_user=$USER \
+           --build-arg nb_uid=$(id -u $USER) \
+           --tag "geobase:local_sample" ./sample
+    exit 0
+}
+
 docker build \
        --build-arg USER_NAME=$USER \
-       --build-arg UID=$UID \
+       --build-arg UID=$(id -u $USER) \
        --tag geobase:local ./local
