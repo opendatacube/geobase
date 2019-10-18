f0177a216c1a187bdcab680c8cbf5f2dfb535e93
Kirill Kouzoubov <kirill888@gmail.com>
2019-10-18 14:39:59 +1100

    Set WORKDIR to HOME, also use `/conf/` dir

--- scripts/run-sample.sh
+++ scripts/run-sample.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 set -eu
-nb_dir="${1:-$(pwd)}"
+nb_dir="${1:-$(pwd)/nb}"
 nb_port="${2:-9988}"
+
+[ -d ${nb_dir} ] || {
+    echo "Creating ${nb_dir}"
+    mkdir -p "${nb_dir}"
+}
+
+echo "Launching: ${nb_dir} ${nb_port}"
 
 docker run \
        -ti --rm \
-       -v ${nb_dir}:/nb \
+       -v ${nb_dir}:"/home/${USER}" \
        -v /run/postgresql:/run/postgresql \
+       -e DATACUBE_CONFIG_PATH="/conf/datacube.conf" \
        -e DB_DATABASE=datacube \
        -p ${nb_port}:9988 \
-       geobase:local_sample $@
+       geobase:local_sample
