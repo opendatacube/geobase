7f2a6670f7462746c1997da81cf069c3a9d9d93a
Kirill Kouzoubov <kirill888@gmail.com>
2019-11-20 17:01:04 +1100

    Build lab extensions as well during bootstrap

--- odc-dev/build_env.sh
+++ odc-dev/build_env.sh
@@ -5,6 +5,8 @@ set -x
 
 env_dir="${PYENV:-/env}"
 pip="${env_dir}/bin/pip"
+jupyter="${env_dir}/bin/jupyter"
+cc="/conf"
 python3 -m venv "${env_dir}"
 
 $pip install -U pip
@@ -13,11 +15,20 @@ $pip install \
     --find-links=/wheels/ \
     --no-cache-dir \
     --no-index \
-    -c /wk/bins.txt \
-    -r /wk/requirements.txt
+    -c "${cc}/bins.txt" \
+    -r "${cc}/requirements.txt"
 
 $pip install \
     --find-links=/wheels/ \
     --no-cache-dir \
-    -c /wk/bins.txt \
-    -r /wk/requirements-odc.txt
+    -c "${cc}/bins.txt" \
+    -r "${cc}/requirements-odc.txt"
+
+while read line; do
+    $jupyter labextension install --no-build $line
+done < "${cc}/lab-extensions.txt"
+$jupyter lab build
+
+while read line; do
+    $jupyter serverextension enable --py $line
+done < "${cc}/lab-server-extensions.txt"
