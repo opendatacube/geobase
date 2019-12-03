c775e656d8d836cb25a4fd6bc245aaeeef7658fe
Kirill Kouzoubov <kirill888@gmail.com>
2019-12-03 14:59:29 +1100

    Use env-build-tool in odc-dev docker

--- odc-dev/build_env.sh
+++ odc-dev/build_env.sh
@@ -7,22 +7,9 @@ env_dir="${PYENV:-/env}"
 pip="${env_dir}/bin/pip"
 jupyter="${env_dir}/bin/jupyter"
 cc="/conf"
-python3 -m venv "${env_dir}"
 
-$pip install -U pip setuptools
-
-$pip install \
-    --find-links=/wheels/ \
-    --no-cache-dir \
-    --no-index \
-    -c "${cc}/bins.txt" \
-    -r "${cc}/requirements.txt"
-
-$pip install \
-    --find-links=/wheels/ \
-    --no-cache-dir \
-    -c "${cc}/bins.txt" \
-    -r "${cc}/requirements-odc.txt"
+env-build-tool new "${cc}/requirements.txt" /wheels "${env_dir}"
+env-build-tool extend "${cc}/requirements-odc.txt" /wheels "${env_dir}"
 
 while read line; do
     $jupyter labextension install --no-build $line
