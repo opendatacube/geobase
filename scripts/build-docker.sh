53e5e5fdff2f723b55d517e251a54d228450da79
Kirill Kouzoubov <kirill888@gmail.com>
2021-05-10 12:05:09 +1000

    Update helper scripts

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,22 +2,5 @@
 
 set -eu
 
-[ "${1-:}" == "base" ] && {
-    # by default pull builder/wheels from docker hub
-    docker build --tag "opendatacube/geobase:builder" ./base/builder
-    docker build --tag "opendatacube/geobase:wheels" ./base/wheels
-    docker build --tag "opendatacube/geobase:runner" ./base/runner
-}
-
-[ "${1-:}" == "sandbox" ] && {
-    docker build \
-           --build-arg nb_user=$USER \
-           --build-arg nb_uid=$(id -u $USER) \
-           --tag "geobase:local_sandbox" ./sandbox
-    exit 0
-}
-
-docker build \
-       --build-arg USER_NAME=$USER \
-       --build-arg UID=$(id -u $USER) \
-       --tag geobase:local ./local
+docker build --tag "opendatacube/geobase-builder:latest" ./base/builder
+docker build --tag "opendatacube/geobase-runner:latest" ./base/runner
