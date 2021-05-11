b79f344e0680de8152d1b8eac7ce44608cfb338a
Kirill Kouzoubov <kirill888@gmail.com>
2021-05-11 10:20:25 +1000

    Updating odc-dev sample image for the new geobase

--- odc/odc-dev/build.sh
+++ odc/odc-dev/build.sh
@@ -4,8 +4,6 @@ image_name="odc-dev"
 nb_port=9988
 
 docker build \
-       --build-arg nb_user=$USER \
-       --build-arg nb_uid=$(id -u $USER) \
        --build-arg nb_port="${nb_port}" \
        --tag "${image_name}" \
        .
